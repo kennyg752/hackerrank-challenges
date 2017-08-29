c2b2629d5c012487756da0815feb15a44550b337
kennyg752 <kennyg752@gmail.com>
2017-08-29 15:18:37 -0700

    Finished Day 2

---
+++ 30-days-of-code/day1-data-types.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# NOTE: not needed in python as variables are implicitly declared.
+# Read and save an integer, double, and String to your variables.
+int2 = int(input())
+double2 = float(input())
+str2 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + int2)
+
+# Print the sum of the double variables on a new line.
+print(d + double2)
+
+# Concatenate and print the String variables on a new line
+print(s + str2)
+# The 's' variable above should be printed first.
